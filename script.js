2111719cc7dd082bb2f015d4ec3adced7faa1c57
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-05 16:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+
+    const freq = {};
+    for (const char of str) {
+        freq[char] = (freq[char] || 0) + 1;
+    }
+    for (const char of str) {
+        if (freq[char] === 1) {
+            return char;
+        }
+    }
+    return null;
 }
-const input = prompt("Enter a string");
+	
+}
+//const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
